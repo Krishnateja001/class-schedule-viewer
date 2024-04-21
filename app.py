fd40e0d99119865741b864f20511f8d0272a23ee
krishnateja <krishnateja.kaspe001@umb.edu>
2024-04-21 19:57:58 -0400

    new version

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from datetime import datetime
 import pandas as pd
 from urllib.parse import quote
 from io import StringIO
-
+from sqlalchemy import create_engine,text
+from maps import generate_map
 app = Flask(__name__)
 
 
@@ -17,6 +18,9 @@ encoded_password = quote("groot@123", safe='')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{encoded_password}@localhost/Lewis_schedule'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+engine=create_engine(f'mysql+mysqlconnector://root:{encoded_password}@localhost/Lewis_schedule')
+connection=engine.connect()
 
 # Define Model for your table
 class Course(db.Model):
@@ -42,7 +46,7 @@ class Course(db.Model):
     instructors = db.Column(db.String(255))
 
 # Route for uploading CSV file
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
         # Check if a file is uploaded
@@ -98,6 +102,105 @@ def upload_csv():
     
     return render_template('upload.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+def class_filters(connection):
+    class_filters=" SELECT DISTINCT course_name FROM course; "
+    sql_query=text(class_filters)
+    cursor = connection.execute(sql_query)
+    course_name = cursor.fetchall()
+
+    class_lis=[]
+    for i in course_name:
+        class_lis.append(i[0])
+    print('HELLLOOO',class_lis[1])
+    return class_lis
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method=='GET':
+        instructor_filters=" SELECT DISTINCT instructors FROM course; "
+        sql_query=text(instructor_filters)
+        connection=engine.connect()
+        # Execute SQL query
+        if connection:
+            cursor = connection.execute(sql_query)
+            instructors = cursor.fetchall()
+
+            lis=[]
+            for i in instructors:
+                lis.append(i[0])
+            class_lis=[]
+            class_lis=class_filters(connection)
+
+            connection.close()
+        return render_template('index_updated.html', instructors=lis,classes=class_lis)
+    
+    if request.method == 'POST':
+        # Extract filter options submitted by the user
+        connection=engine.connect()
+        class_name = request.form.get('class','')
+        semester = request.form.get('semester','')
+        instructor=request.form.get('instructor','')
+        level=request.form.get('level','')
+        # Extract other filter options as needed
+
+        # Construct SQL query
+
+
+        query = """
+            SELECT course_name,course_code,campus,level,start_time,end_time,days,location,lecture_type,instructors
+            FROM course
+        """
+
+        # Add conditions based on filters
+        conditions = []
+        if class_name:
+            conditions.append(f"course_name = '{class_name}'")
+        if semester:
+            conditions.append(f"semester = '{semester}'")
+        if instructor:
+            conditions.append(f"instructors = '{instructor}'")
+        if level:
+            conditions.append(f"level = '{level}'")
+        if conditions:
+            query += " WHERE " + " AND ".join(conditions)
+
+        query += """
+            GROUP BY campus,course_name
+            ORDER BY campus;
+        """
+
+        sql_query=text(query)
+        # Execute SQL query
+        print(sql_query)
+        if connection:
+            cursor = connection.execute(sql_query)
+
+            class_schedules = cursor.fetchall()
+           
+
+        instructor_filters=" SELECT DISTINCT instructors FROM course; "
+        sql_query=text(instructor_filters)
+        # Execute SQL query
+        if connection:
+            cursor = connection.execute(sql_query)
+            instructors = cursor.fetchall()
+            print(instructors)
+            lis=[]
+            for i in instructors:
+                lis.append(i[0])
+            
+            class_lis=class_filters(connection)
+            connection.close()
+            map_plot=generate_map(['Oak Brook','Albuquerque','College of DuPage'])
+            # Render template with filtered class schedules
+            return render_template('index_updated.html', class_schedules=class_schedules,instructors=lis,classes=class_lis,plot_div=map_plot)
+
+    # If request method is GET or filters not submitted, render the initial template
+    return render_template('index_updated.html', class_schedules=None)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
--- maps.py
+++ maps.py
@@ -1,40 +1,84 @@
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine,text
-from urllib.parse import quote
+from flask import Flask, render_template, request,send_file
+import plotly.graph_objs as go
+import  io
+
 app = Flask(__name__)
 
-# Encode the password
-encoded_password = quote("groot@123", safe='')
+Lewisuni_coordinates={
+    "Romeoville": {
+        "latitude": 41.60687051879109,
+        "longitude": -88.07983952694718
+    },
+    "Oak Brook": {
+        "latitude": 41.849704827990585,
+        "longitude": -87.94512755057589
+    },
+    "Albuquerque": {
+        "latitude": 35.107204505190644,
+        "longitude": -106.5675698175947
+    },
+    "College of DuPage": {
+        "latitude": 41.84120967960425,
+        "longitude": -88.07337229024918
+    },
+    "Joliet Junior College": {
+        "latitude": 41.51043180472986,
+        "longitude": -88.17855687273331
+    },
+    "Hickory Hills": {
+        "latitude": 41.716190756723314,
+        "longitude": -87.82041653073465
+    },
+    "Tinely Park": {
+        "latitude": 41.55518054280682,
+        "longitude": -87.7934635460868
+    },
+    "Wiley Delasalle Institute": {
+        "latitude": 41.83190967445167,
+        "longitude": -87.62348654986252
+    }
+}
 
-# Configure MySQL database connection
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{encoded_password}@localhost/Lewis_schedule'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# Session = sessionmaker(bind=engine)
-engine=create_engine(f'mysql+mysqlconnector://root:{encoded_password}@localhost/Lewis_schedule')
-connection=engine.connect()
-@app.route('/')
-def index():
-    return render_template('lewis_map.html')
 
-@app.route('/schedule', methods=['POST'])
-def show_schedule():
-    campus = request.form['campus']
-    instructor = request.form['instructor'] 
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
+# @app.route('/map', methods=['POST'])
+def generate_map(places):
+    # Retrieve latitude and longitude data from the request
+    traces = []
+    for place in places:
+        latitude= round(Lewisuni_coordinates[place]['latitude'], 6)
+        longitude=round(Lewisuni_coordinates[place]['longitude'], 6)
+        print(latitude,longitude)
+    # Create a scattermapbox trace
+        trace = go.Scattermapbox(
+            lat=[latitude],
+            lon=[longitude],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=14
+            ),
+          name=place
+        )
+        traces.append(trace)
+    
 
-    sql_query =text("SELECT * FROM course WHERE course.campus = :campus group by course.campus")
+    # Create the layout for the map
+    layout = go.Layout(
+        autosize=True,
+        mapbox=dict(
+            center=dict(lat=41.60687051879109, lon=-88.07983952694718),
+            style='open-street-map',
+            zoom=10
+        )
+    )
 
-    # Execute the SQL query with parameters
-    cursor=connection.execute(sql_query,{"campus":campus+' campus'})
+    # Create the figure
+    fig = go.Figure(data=traces, layout=layout)
+    # image_path = 'map_image.png'
+    # fig.write_image(image_path, format='png')
+    # return None
 
-    # Fetch all rows
-    rows = cursor.fetchall()
-    connection.close()
+    plot_div = fig.to_html(full_html=False)
+    return plot_div
 
-    return render_template('schedule.html', schedules=rows)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# generate_map(places=Lewisuni_coordinates.keys())
